349f1cd2170e98e9e24c2c22eeb9d2af40c44eb5
jraleman <jraleman@bendiburg.com>
2021-11-06 16:21:00 -0700

    Implements `FlashCard` flip functionality

--- src/components/FlashCard/FlashCard.jsx
+++ src/components/FlashCard/FlashCard.jsx
@@ -1,23 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import RenderAnswers from './RenderAnswers';
 
-const FlashCard = ({ question, answers }) => (
-    <CardContainer>
-        <CardTitle>{question}</CardTitle>
-        <OrderedList>
-            <RenderAnswers>{answers}</RenderAnswers>
-        </OrderedList>
-    </CardContainer>
-);
+const FlashCard = ({ question, answers }) => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleOnClick = () => {
+        setIsFlipped((t) => !t);
+    }
+    return (
+        <CardContainer onClick={handleOnClick}>
+            <CardInner isFlipped={isFlipped}>
+                <FrontCard>
+                    <CardTitle>{question}</CardTitle>
+                </FrontCard>
+                <BackCard>
+                    <OrderedList>
+                        <RenderAnswers>{answers}</RenderAnswers>
+                    </OrderedList>
+                </BackCard>
+            </CardInner>
+        </CardContainer>
+    );
+};
 
 const CardContainer = styled.div`
-    background-color: #f9f9f9;
     padding: 1em;
-    border: #666666 solid;
     max-width: 768px;
     margin: 0 auto;
     margin-bottom: 1em;
+    perspective: 1000px;
+    height: 300px;
+`;
+
+const CardInner = styled.div`
+    background-color: #f9f9f9;
+    border: #666666 solid;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+    ${({ isFlipped }) => isFlipped && 'transform: rotateY(180deg)'}
+`;
+
+const FrontCard = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden; /* Safari */
+    backface-visibility: hidden;
+`;
+
+const BackCard = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden; /* Safari */
+    backface-visibility: hidden;
+    transform: rotateY(180deg);
 `;
 
 const CardTitle = styled.h4`
