9f3f8469c55b8e61bdf8a41ab6579050a46b26b5
jraleman <jraleman@bendiburg.com>
2021-11-06 15:33:02 -0700

    Updates `Sections` styles

--- src/containers/Sections/RenderSection.jsx
+++ src/containers/Sections/RenderSection.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import FlashCard from '../../components/FlashCard';
 import uniqueId from '../../utils/helpers/uniqueId';
 
 const RenderSection = ({ title = '', content = [] }) => (
-    <div>
-        <h3>{title}</h3>
-        {content.map((d) => (
+    <SectionContainer>
+        <SectionTitle>{title}</SectionTitle>
+        {content.map((c) => (
             <FlashCard
                 key={uniqueId('flash-card_')}
-                question={Object.keys(d)}
-                answers={Object.values(d)[0]}
+                question={Object.keys(c)}
+                answers={Object.values(c)[0]}
             />
         ))}
-        <hr />
-    </div>
+        <SectionDivider />
+    </SectionContainer>
 );
+
+const SectionTitle = styled.h3`
+    text-align: center;
+    text-transform: uppercase;
+`;
+
+const SectionContainer = styled.div`
+    padding: 2em;
+`;
+
+const SectionDivider = styled.hr``;
 
 export default RenderSection;
--- src/containers/Sections/Sections.jsx
+++ src/containers/Sections/Sections.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 import RenderSection from './RenderSection';
 import uniqueId from '../../utils/helpers/uniqueId';
 
 const Sections = ({ data = [] }) => {
     const title = "Questions and Answers";
     return (
-        <div>
-            <h2>{title}</h2>
+        <SectionsContainer>
+            <Title>{title}</Title>
             {data.map(({ title, content }) => (
                 <RenderSection
                     key={uniqueId('section_')}
@@ -14,8 +15,15 @@ const Sections = ({ data = [] }) => {
                     content={content}
                 />
             ))}
-        </div>
-    )
+        </SectionsContainer>
+    );
 };
+
+const Title = styled.h2`
+    text-align: center;
+    text-transform: uppercase;
+`;
+
+const SectionsContainer = styled.div``;
 
 export default Sections;
