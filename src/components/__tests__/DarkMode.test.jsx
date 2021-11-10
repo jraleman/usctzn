65b7422751f6f556d1ff9b46bb268261fa2d2ff5
jraleman <jraleman@bendiburg.com>
2021-11-09 17:17:26 -0800

    Updates `DarkMode` test

--- src/components/__tests__/DarkMode.test.jsx
+++ src/components/__tests__/DarkMode.test.jsx
@@ -4,8 +4,24 @@ import DarkMode from '../DarkMode';
 
 describe('DarkMode', () => {
     it('renders', async () => {
-        const component = render(<DarkMode  />);
+        const onThemeChange = jest.fn();
+        const theme = '';
+        const component = render(
+            <DarkMode onThemeChange={onThemeChange} theme={theme} />
+        );
         expect(component).not.toBeNull();
         expect(component).toMatchSnapshot();
+    });
+
+    it('changes theme mode', async () => {
+        const onThemeChange = jest.fn();
+        const theme = 'light';
+        const component = render(
+            <DarkMode onThemeChange={onThemeChange} theme={theme} />
+        );
+        // fireevent click div
+        // get from query
+        // expect onClick
+        expect(component).not.toBeNull();
     });
 });
